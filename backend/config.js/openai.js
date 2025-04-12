974bdf123b85a0525eb0b4e9a30dc78c88211f10
httpsyash <yashk19000@gmail.com>
2025-04-12 19:16:18 +0530

    first commit

--- backend/config.js/openai.js
+++ backend/config.js/openai.js
@@ -2,9 +2,10 @@ import OpenAI from "openai";
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log(import.meta.env.TEST_VARIABLE)
 const openai = new OpenAI({
     
-  apiKey: process.env.TEST_VARIABLE,
+  apiKey: import.meta.env.TEST_VARIABLE,
   baseURL: "https://openrouter.ai/api/v1", 
 });
 
